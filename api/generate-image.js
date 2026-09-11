600e2af3073b53445fe2fb0e646e95951b6a5d9e
DarkAi-project <Mohammedqandah0@gmail.com>
2026-09-12 00:27:57 +0300

    Update generate-image.js

--- api/generate-image.js
+++ api/generate-image.js
@@ -19,7 +19,8 @@ export default async function handler(req, res) {
       return res.status(500).json({ error: 'مفتاح Hugging Face غير مُعرَّف على السيرفر' });
     }
 
-    const modelUrl = 'https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-schnell';
+    const modelUrl = 'https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-dev';
+
 
     const imgRes = await fetch(modelUrl, {
       method: 'POST',
